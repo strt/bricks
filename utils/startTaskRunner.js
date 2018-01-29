75b5c6faa0c67ad78e8c0cf36557d08b57a9ee80
Alexander Nanberg <alex.nanberg@hotmail.com>
2018-01-29 07:58:02 +0100

    Remove unnecessary log

--- utils/startTaskRunner.js
+++ utils/startTaskRunner.js
@@ -5,10 +5,12 @@ const log = require('./log');
 require('../config/gulpfile');
 
 gulp.on('start', (event) => {
+  if (event.name === '<parallel>') return;
   log.gulp(`Starting ${chalk.cyan(event.name)}...`);
 });
 
 gulp.on('stop', (event) => {
+  if (event.name === '<parallel>') return;
   const time = prettyTime(event.duration);
   log.gulp(`Finished ${chalk.cyan(event.name)} after ${chalk.magenta(time)}`);
 });
