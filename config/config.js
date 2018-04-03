31e92d5290f31ab00d87228e642d6aa4acbd7958
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-04-03 09:30:57 +0200

    Convert files to arrow fn

--- config/config.js
+++ config/config.js
@@ -27,15 +27,13 @@ const defaultConfig = {
       app: './app.js',
     },
   },
-  files(config = this) {
-    return [
-      `${config.source}/**`,
-      `!${config.source}/${config.images.path}{,/**}`,
-      `!${config.source}/${config.icons.path}{,/**}`,
-      `!${config.source}/${config.styles.path}{,/**}`,
-      `!${config.source}/${config.scripts.path}{,/**}`,
-    ];
-  },
+  files: config => [
+    `${config.source}/**`,
+    `!${config.source}/${config.images.path}{,/**}`,
+    `!${config.source}/${config.icons.path}{,/**}`,
+    `!${config.source}/${config.styles.path}{,/**}`,
+    `!${config.source}/${config.scripts.path}{,/**}`,
+  ],
   browserSync: {
     notify: false,
     open: false,
