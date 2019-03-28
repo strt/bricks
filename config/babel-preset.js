b0f57c3ad0862e4c0ccd4a3cae1add5b8e7cc43b
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-28 08:49:51 +0100

    feat: add transform-runtime plugin and optimize config

--- config/babel-preset.js
+++ config/babel-preset.js
@@ -1,18 +1,28 @@
-module.exports = () => ({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: false,
-      },
+module.exports = env => {
+  const isEnvDevelopment = env === 'development';
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'entry',
+          corejs: 3,
+          exclude: ['transform-typeof-symbol'],
+        },
+      ],
+      [
+        '@babel/preset-react',
+        { useBuiltIns: true, development: isEnvDevelopment },
+      ],
     ],
-    ['@babel/preset-react', { useBuiltIns: true }],
-  ],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-    '@babel/plugin-syntax-dynamic-import',
-    'macros',
-  ],
-});
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+      ['@babel/plugin-transform-runtime', { useESModules: true }],
+      '@babel/plugin-syntax-dynamic-import',
+      'macros',
+    ],
+  };
+};
